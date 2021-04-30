19acc9b8a5f911ec074ce6c157ff8fb5e6002dc2
Cesar-Acevedo23 <ces.acevedo.23@gmail.com>
2021-04-30 14:44:47 -0400

    Creando archivos simple counter

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,12 +1,25 @@
-
+import {useRef, useState} from 'react'
 import './App.css';
 
 function App() {
+  
+  let [contador1, updateContador1] = useState(0);
+  
+  setInterval(() =>{
+    updateContador1(contador1 + 1);
+    if(contador1 >= 9){
+      updateContador1(0);
+    }
+  },1000)
+  
   return (
     <div className="App">
-      <h1></h1>
+      
+      <h1>{contador1}</h1>
+      <h2>Valor del contador</h2>
     </div>
   );
+
 }
 
 export default App;
